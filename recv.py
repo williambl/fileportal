c89c82f2581112c71d753283994a4861b235e191
williambl <william@williambl.com>
2017-07-08 15:21:26 +0100

    File name now done automatically

--- recv.py
+++ recv.py
@@ -5,7 +5,11 @@ def recv(ip, path):
 
     s.connect((ip, 25565))
 
-    f = open(path,'wb')
+    filename = s.recv(1024)
+
+    f = open(filename.decode('utf-8'),'wb')
+
+    s.send("send it".encode('utf-8'))
 
     while True:
         data = s.recv(1024)
--- send.py
+++ send.py
@@ -10,6 +10,11 @@ def send(ip, path):
     csocket, addr = s.accept()
     print('Got connection from', addr)
 
+    csocket.send(f.name.encode('utf-8'))
+
+    while True:
+        if (csocket.recv(1024).decode('utf-8') == "send it"): break
+
     while True:
         data = f.read(1024)
         if (len(data)):
